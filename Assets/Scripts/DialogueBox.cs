c7287efb1920cc41169ebac2c65414554f5ec154
Will <wctaplin@gmail.com>
2020-05-20 17:34:48 -0400

    Fixed font scaling at multiple resoultions

---
+++ Assets/Scripts/DialogueBox.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueBox : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject sprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPos = Camera.main.WorldToScreenPoint(sprite.transform.position);
+        float x = Mathf.Ceil(newPos.x);
+        float y = Mathf.Ceil(newPos.y);
+        GetComponent<RectTransform>().position = new Vector3(x, y, 1);
+        //GetComponent<RectTransform>().position = Camera.main.WorldToScreenPoint(sprite.transform.position);
+    }
+}
---
+++ Assets/Scripts/GetScaleFactor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetScaleFactor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 1080p - 6
+        // 720p - 4
+        // 1600x900 - 5 <- PROBLEM
+        float scale = Screen.currentResolution.width / 320;
+        if (scale % 2 != 0)
+        {
+            scale++;
+        }
+        gameObject.GetComponent<CanvasScaler>().scaleFactor = scale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
